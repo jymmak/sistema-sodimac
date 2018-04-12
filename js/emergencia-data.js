618056abd0a7754fbd439b71936bc419116e7f05
andreatelleria <andrea.telleria@gmail.com>
2018-04-12 06:06:04 -0500

    Agregando data para sistema de emergencia

---
+++ js/emergencia-data.js
@@ -0,0 +1,60 @@
+const data = [
+  {
+    numero: 1,
+    estado: 'cerrado',
+    historial: [
+      {
+        date: '12-01-2018',
+        time: '3:00 PM'
+      },
+      {
+        date: '23-03-2017',
+        time: '11:00 AM'
+      }
+    ]
+  },
+  {
+    numero: 2,
+    estado: 'cerrado',
+    historial: [
+      {
+        date: '03-06-2017',
+        time: '3:00 PM'
+      },
+      {
+        date: '23-03-2017',
+        time: '11:00 AM'
+      }
+    ]
+  },
+  {
+    numero: 3,
+    estado: 'abierto',
+    historial: [
+      {
+        date: '12-04-2018',
+        time: '9:00 AM'
+      },
+      {
+        date: '23-03-2017',
+        time: '11:00 AM'
+      }
+    ]
+  },
+  {
+    numero: 4,
+    estado: 'cerrado',
+    historial: [
+      {
+        date: '22-08-2017',
+        time: '1:00 PM'
+      },
+      {
+        date: '23-03-2017',
+        time: '11:00 AM'
+      }
+    ]
+  }
+];
+
+console.log(data);
